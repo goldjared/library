9f66f4821d2bfe1089a0e71ec9c18d989d7d96ee
jared <114547220+goldjared@users.noreply.github.com>
2023-02-08 20:40:35 -0500

    Create book constructor

---
+++ main.js
@@ -0,0 +1,18 @@
+let myLibrary = [];
+
+function book(title, author, pages, read) {
+  this.title = title
+  this.author = author
+  this.pages = pages
+  this.read = read
+  this.info = function() {
+    return `${this.title}, ${this.author}, ${this.pages}, ${this.read}`;
+  };
+};
+
+const book1 = new book('The Hobbit', 'J.R.R. Tolkien', 300, 'Read');
+const book2 = new book('Harry Potter', 'J.K. Rowling', 550, 'Read');
+
+function addBookToLibrary(add) {
+ const newBook = new book
+}
