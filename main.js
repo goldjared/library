e269928c525bdbe457041a25c5442d89f482c59a
jared <jaredgold@proton.me>
2023-02-09 13:14:51 -0500

    Rework js, use foreach to add books to display.

--- main.js
+++ main.js
@@ -1,18 +1,37 @@
 let myLibrary = [];
 
-function book(title, author, pages, read) {
+
+function Book(title, author, pages, read) {
   this.title = title;
   this.author = author;
   this.pages = pages;
   this.read = read;
-  this.info = function () {
-    return `${this.title}, ${this.author}, ${this.pages}, ${this.read}`;
+  this.info = function() {
+    return `${this.title} by ${this.author}, ${this.pages}, ${this.read}`;
   };
 }
 
-const book1 = new book("The Hobbit", "J.R.R. Tolkien", 300, "Read");
-const book2 = new book("Harry Potter", "J.K. Rowling", 550, "Read");
+const book1 = new Book('The Hobbit', 'J.R.R Tolkien', 300, 'Read');
+const book2 = new Book('The Hobbitses', 'J.R.R Tolkiensss', 300, 'Read');
 
-function addBookToLibrary() {
-  myLibrary.push(book);
+function addBookToLibrary(info) {
+  myLibrary.push(info)
+};  
+
+addBookToLibrary(book1);
+addBookToLibrary(book2);
+
+
+function displayBook(array) {
+  const container = document.querySelector('.container');
+  array.forEach(obj => {
+    const bookItem = document.createElement('div');
+    bookItem.textContent = obj.info();
+    container.appendChild(bookItem);
+    console.log(obj.info());
+  });
+
 }
+
+displayBook(myLibrary);
+
